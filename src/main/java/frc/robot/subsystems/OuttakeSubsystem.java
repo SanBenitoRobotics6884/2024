8bb5dec3c956acb239617648195cdfccd4ab34d9
ROBOTICS-02 <ROBOTICS-02@sbhsd.k12.ca.us>
2024-02-05 18:25:41 -0800

    invert right motor

--- src/main/java/frc/robot/subsystems/OuttakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/OuttakeSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -17,15 +18,19 @@ public class OuttakeSubsystem extends SubsystemBase {
   CANSparkMax m_shooterMotorI = new CANSparkMax(1, MotorType.kBrushless);
   CANSparkMax m_shooterMotorII = new CANSparkMax(2, MotorType.kBrushless);
   CANSparkMax m_pivotMotor = new CANSparkMax(3, MotorType.kBrushless);
+  
+  PIDController m_pid = new PIDController(0, 0, 0);
 
   /** Creates a new OuttakeSubsystem. */
   public OuttakeSubsystem(XboxController controller) {
     m_controller = controller;
     m_shooterMotorI.follow(m_shooterMotorII);
+    m_shooterMotorII.setInverted(true);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
   }
 }
