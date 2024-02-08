853fd6dc36915f9908b9e57ba3f977c3be8c1f4a
Voyager6884 <dsr6884voyager@gmail.com>
2024-02-07 19:01:53 -0800

    add new methods for DeployIntakeCommand, remove small code

--- src/main/java/frc/robot/commands/DeployIntakeCommand.java
+++ src/main/java/frc/robot/commands/DeployIntakeCommand.java
@@ -5,27 +5,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class DeployCommand extends Command {
+public class DeployIntakeCommand extends Command {
   IntakeSubsystem m_intakesubsystem;
   /** Creates a new DeployCommand. */
-  public DeployCommand(IntakeSubsystem subsystem) {
+  
+  public DeployIntakeCommand(IntakeSubsystem subsystem) {
     m_intakesubsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
+
+
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    m_intakesubsystem.deployGoal();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    System.out.println("Reached goal");
   }
 
   // Called once the command ends or is interrupted.
@@ -37,6 +41,6 @@ public class DeployCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_intakesubsystem.deployHitGoal();
   }
 }
