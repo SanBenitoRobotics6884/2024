ebe4e079d75cd8157d5a74ff197bd79805356486
ROBOTICS-02 <ROBOTICS-02@sbhsd.k12.ca.us>
2024-02-07 19:02:53 -0800

    put pid loops in periodic method

--- src/main/java/frc/robot/commands/ClimbCommands.java
+++ src/main/java/frc/robot/commands/ClimbCommands.java
@@ -6,20 +6,22 @@ import frc.robot.subsystems.ClimbSubsystem;
 
 public class ClimbCommands extends Command {
     private ClimbSubsystem m_climbSubsystem;
-    private final Joystick m_climbJoystick;
-    private final int m_climbButton;
+    private Joystick m_climbJoystick;
+    private int m_climbButton;
 
 
-    public void ClimbCommand(ClimbSubsystem climbSubsystem, Joystick climbJoystick, int m_climbButton) {
+    public ClimbCommands(ClimbSubsystem climbSubsystem, Joystick climbJoystick, int m_climbButton) {
         this.m_climbSubsystem = climbSubsystem;
         this.m_climbJoystick = climbJoystick;
         this.m_climbButton = m_climbButton;
         addRequirements(climbSubsystem);
+    }
+    @Override
+    public void execute() {
+        if (m_climbJoystick.getRawButton( m_climbButton))
+            m_climbSubsystem.toggleClimb();
+        else 
+            m_climbSubsystem.stopClimb();
+    }
 }
-@Override
-public void execute() {
-    if (m_climbJoystick.getRawButton(10))
-        m_climbSubsystem.toggleClimb();
-    else 
-        m_climbSubsystem.stopClimb();
-}
+     
--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -7,33 +7,38 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase{
+ 
   /** Creates a new ClimbSubsystem. */
     private CANSparkMax m_rightClimbMotor = new CANSparkMax(0,MotorType.kBrushless);
     private CANSparkMax m_leftClimbMotor = new CANSparkMax(0,MotorType.kBrushless);
     
     private PIDController m_rightPIDController = new PIDController(0, 0, 0);
     private PIDController m_leftPIDController = new PIDController(0, 0, 0);
-
+        
     private RelativeEncoder m_rightClimbEncoder;
     private RelativeEncoder m_leftClimbEncoder;
 
-    private double m_rightSetPoint;
-    private double m_leftSetPoint;
+    
 
     private boolean isExtended = false;
-     
+    
 
     public ClimbSubsystem () {
     m_rightClimbEncoder = m_rightClimbMotor.getEncoder();
     m_leftClimbEncoder = m_leftClimbMotor.getEncoder();
     m_rightClimbEncoder.setPosition(0);
     m_leftClimbEncoder.setPosition(0);
-    }
+    m_rightClimbMotor.setIdleMode(IdleMode.kBrake);
+    m_leftClimbMotor.setIdleMode(IdleMode.kBrake);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
@@ -45,9 +50,10 @@ public class ClimbSubsystem extends SubsystemBase{
 
     double m_rightMotorOutput = m_rightPIDController.calculate(m_rightCurrentHeight);
     double m_leftMotorOutput = m_leftPIDController.calculate(m_leftCurrentHeight);
-
+   
     m_rightMotorOutput = Math.min(0, Math.max(0, 0));
     m_leftMotorOutput = Math.min(0, Math.max(0, 0));
+    m_rightClimbMotor.set(MathUtil.clamp(2, 1, 1.5)); 
     
   }
 
@@ -62,6 +68,8 @@ public void toggleClimb() {
 
     double m_rightMotorOutput = m_rightPIDController.calculate(0);
     double m_leftMotorOutput= m_leftPIDController.calculate(0);
+      m_rightPIDController.setSetpoint(0);
+    m_leftPIDController.setSetpoint(0);
 
     m_rightClimbMotor.set(m_rightMotorOutput);
     m_leftClimbMotor.set(m_leftMotorOutput);
@@ -71,7 +79,9 @@ public void toggleClimb() {
   public void retract() {   
     double m_rightMotorOutput = m_rightPIDController.calculate(0);
     double m_leftMotorOutput = m_leftPIDController.calculate(0);
-   
+     m_rightPIDController.setSetpoint(0);
+    m_leftPIDController.setSetpoint(0);
+
    
     m_rightClimbMotor.set(m_rightMotorOutput);
     m_leftClimbMotor.set(m_leftMotorOutput);
@@ -80,9 +90,12 @@ public void toggleClimb() {
   public void stopClimb(){
     m_rightClimbMotor.set(0);
     m_leftClimbMotor.set(0);
-   }
-}
    
+
+
+
+  }
+}
   
 
   
