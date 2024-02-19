8e3920bd54aeb774fd4d9904619ede443e333a28
zachypoo <147222946+TeddyRex@users.noreply.github.com>
2024-02-19 14:56:12 -0800

    Intake (#1)
    * init intake
    
    * add smartcurrent and speed
    
    * implement picking up note and switch relative encoder and use kBrake for idle mode
    
    * make and add intake constants, and finish reelCommand work on profiledPID and deploy
    
    * add constants
    
    * add reverse deploy command, clean up reel method
    
    * add more constants for velocity and accelerations, add more commands and remove
    unneeded code
    
    * add more constants for pivot changed code a tiny bit
    
    * add constants, change method name
    
    * add new methods for DeployIntakeCommand, remove small code
    
    * add new methods for pivot, neat out code
    
    * calculate goal in periodic, etc
    
    * add tolerance, remove and fix parts of code that are unneeded/irrelevant, etc, finish deployintakecommand
    
    * finish code, add final touches soon
    
    * small change, not fully finish robotcontainer but done with rest of code
    
    * finish code, including RobotContainer
    
    * new constants, rename commands, fix code
    
    * reset pid
    
    * fix small errors
    
    ---------
    
    Co-authored-by: ROBOTICS-03 <ROBOTICS-03@sbhsd.k12.ca.us>
    Co-authored-by: Voyager6884 <dsr6884voyager@gmail.com>
    Co-authored-by: TeddyRex <lozano.36644@sbhsd.k12.ca.us>
    Co-authored-by: Lucas Fowler <115597025+LucasF6@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,121 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.Intake.*;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  
+
+  private final CANSparkMax m_intakeMotor = new CANSparkMax(INTAKE_MOTOR_ID, MotorType.kBrushless);
+  private final CANSparkMax m_pivotMotor = new CANSparkMax(PIVOT_MOTOR_ID, MotorType.kBrushless);
+  
+    //double kP = 0;
+    //double kI = 0;    SET THE VARIABLES TO THE CONSTANTS. :fire: :skull:
+    //double kD = 0;
+
+  public ProfiledPIDController m_pivotPID =
+          new ProfiledPIDController(PIVOT_kP, PIVOT_kI, PIVOT_kD, new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION));
+  private  DigitalInput m_limitSwitch = new DigitalInput(LIMIT_SWITCH);
+
+  private RelativeEncoder m_intakeEncoder;
+  private RelativeEncoder m_pivotEncoder;
+ 
+  /** Creates a new Intake. */
+  public IntakeSubsystem() {
+
+    m_intakeMotor.restoreFactoryDefaults();
+    m_intakeMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    //m_intakeMotor.setSmartCurrentLimit(0); UNNEEDED ATM
+    m_intakeEncoder = m_intakeMotor.getEncoder();
+
+    m_pivotMotor.restoreFactoryDefaults();
+    m_pivotMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    //m_pivotMotor.setSmartCurrentLimit(0); UNNEEDED ATM
+    m_pivotEncoder = m_pivotMotor.getEncoder();
+    m_pivotEncoder.setPosition(ENCODER_POSITION);
+    
+    m_pivotPID.setTolerance(TOLERANCE);
+        /**
+      Saw this in cranberry code, it sets like current limit in AMPs though, I don't know what it is so LUCAS help.
+    */
+
+    //dunno if this is useful atm but we'll see
+
+    m_pivotPID.reset(m_pivotEncoder.getPosition(), m_pivotEncoder.getVelocity());
+  }
+
+ @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+    if (m_pivotPID.atGoal()) {
+      m_pivotMotor.setVoltage(VOLTS);
+    } else {
+      m_pivotMotor.set(m_pivotPID.calculate(m_pivotEncoder.getPosition()));
+    }
+  }
+
+  //We set the goal/setpoint to the PID
+  public void deploy() {
+    m_pivotPID.setGoal(DEPLOY_SETPOINT);
+    
+  }
+
+  //Returns boolean of whather PID is at goal/setpoint.
+  public boolean atSetpoint() {
+    return m_pivotPID.atGoal();
+    }
+
+  
+  // Set's speed to intake motor to reel
+  public void reel() {
+    m_intakeMotor.set(INTAKE_MOTOR_SPEED);
+  }
+
+  // Returns whether limitswitch is triggered or not
+  public boolean noteHeld() {
+    return m_limitSwitch.get();
+  }
+
+  //Stops intake motor from spinning 
+  public void rollerStop() {
+      m_intakeMotor.stopMotor();
+  }
+
+  public void spit() {
+    m_intakeMotor.set(INTAKE_MOTOR_SPIT_SPEED);
+  }
+
+  public boolean noteGone() {
+    if (m_limitSwitch.get() == true) {
+      return false;
+    } else if(m_limitSwitch.get() == false) {
+      return true;
+    }
+    return false;
+  }
+
+  public void stow() {
+    m_pivotPID.setGoal(STOW_SETPOINT);
+  }
+
+
+
+  
+  
+}
