123de8d6290c7ffc90d67441b76bd75b09129058
232009sketch <125757406+232009sketch@users.noreply.github.com>
2024-02-19 13:49:21 -0800

    Outtake (#3)
    * push to continue outtake code
    
    * Add the outtake command folder
    
    * invert right motor
    
    * Code PID of Outtake
    
    * Create outtake commands and button bindings
    
    * Add rotate command methods
    
    * remove unused code
    
    * explain what is the pourpose of the commands on the subsystem
    
    ---------
    
    Co-authored-by: Macbook <sbhs6884@gmail.com>
    Co-authored-by: Voyager6884 <dsr6884voyager@gmail.com>
    Co-authored-by: ROBOTICS-02 <ROBOTICS-02@sbhsd.k12.ca.us>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -70,4 +70,28 @@ public final class Constants {
     public static final Translation2d BL_LOCATION = new Translation2d(-APOTHEM, APOTHEM);
   }
 
+  public static final class Outtake {
+    public static final int TAKE_NOTE_MOTOR_ID = 1;
+    public static final int SHOOTER_MOTOR_I_ID = 2;
+    public static final int SHOOTER_MOTOR_II_ID = 3;
+    public static final int PIVOT_MOTOR_ID = 4;
+
+    public static final boolean SHOOTER_MOTOR_I_INVERTED = true;
+
+    public static final double PIVOT_kP = 0;
+    public static final double PIVOT_kI = 0;
+    public static final double PIVOT_kD = 0;
+
+    public static final double SPEAKER_POSITION = 0;
+    public static final double AMP_POSITION = 1; 
+
+    public static final double SHOOTER_SPEAKER_MOTOR_VOLTAGE = 1;
+    public static final double TAKE_NOTE_SPEAKER_MOTOR_VOLTAGE = 1;
+    public static final double SHOOTER_AMP_MOTOR_VOLTAGE = 0;
+    public static final double TAKE_NOTE_AMP_MOTOR_VOLTAGE = 0;
+    public static final double YOINK_TAKE_NOTE_SPEED = 0;
+    public static final double YOINK_SHOOTERS_SPEED = 0;
+
+    public static final double TOLERANCE = 0.2;
+  }
 }
