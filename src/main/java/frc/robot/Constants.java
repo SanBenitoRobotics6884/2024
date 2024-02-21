bc017848835281d67cde4419be8a58a5b7f4fbb5
Voyager6884 <dsr6884voyager@gmail.com>
2024-02-21 15:26:52 -0800

    Modify climb code to match change of climb

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,11 +74,11 @@ public final class Constants {
     public static final double MAX_UP_VOLTAGE = 0.4; 
     public static final double MAX_DOWN_VOLTAGE = -0.4; 
     
-    public static final double EXTEND_MOTOR_SETPIONT = 0.9; 
-    public static final double RETRACT_MOTOR_SETPOINT = 0.9;
+    public static final double EXTEND_MOTOR_SETPOINT = 0.9; // this is where climber starts
+    public static final double RETRACT_MOTOR_SETPOINT = 0;
     
-    public static final int RCLIMB_MOTOR_ID = 14;
-    public static final int LCLIMB_MOTOR_ID = 15;
+    public static final int R_CLIMB_MOTOR_ID = 14;
+    public static final int L_CLIMB_MOTOR_ID = 15;
     
     public static final double CLIMB_kP = 0;
     public static final double CLIMB_kI = 0;
--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -17,9 +17,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.Climb.*;
 
 public class ClimbSubsystem extends SubsystemBase{
-  /** Creates a new ClimbSubsystem. */
-  private CANSparkMax m_rightClimbMotor = new CANSparkMax(RCLIMB_MOTOR_ID,MotorType.kBrushless);
-  private CANSparkMax m_leftClimbMotor = new CANSparkMax(LCLIMB_MOTOR_ID,MotorType.kBrushless);
+  private CANSparkMax m_rightClimbMotor = new CANSparkMax(R_CLIMB_MOTOR_ID,MotorType.kBrushless);
+  private CANSparkMax m_leftClimbMotor = new CANSparkMax(L_CLIMB_MOTOR_ID,MotorType.kBrushless);
   
   private PIDController m_rightPIDController = new PIDController(CLIMB_kP, CLIMB_kI, CLIMB_kD);
   private PIDController m_leftPIDController = new PIDController(CLIMB_kP, CLIMB_kI, CLIMB_kD);
@@ -27,11 +26,16 @@ public class ClimbSubsystem extends SubsystemBase{
   private RelativeEncoder m_rightClimbEncoder;
   private RelativeEncoder m_leftClimbEncoder;
 
+  private boolean m_isRetracting = false;
+
+  /** Creates a new ClimbSubsystem. */
   public ClimbSubsystem() {
     m_rightClimbEncoder = m_rightClimbMotor.getEncoder();
     m_leftClimbEncoder = m_leftClimbMotor.getEncoder();
     m_rightClimbEncoder.setPosition(0);
     m_leftClimbEncoder.setPosition(0);
+    m_leftPIDController.setSetpoint(RETRACT_MOTOR_SETPOINT);
+    m_rightPIDController.setSetpoint(RETRACT_MOTOR_SETPOINT);
     m_rightClimbMotor.setIdleMode(IdleMode.kBrake);
     m_leftClimbMotor.setIdleMode(IdleMode.kBrake);
   }
@@ -43,21 +47,23 @@ public class ClimbSubsystem extends SubsystemBase{
     
     double m_rightMotorOutput = m_rightPIDController.calculate(m_rightCurrentHeight);
     double m_leftMotorOutput = m_leftPIDController.calculate(m_leftCurrentHeight);
-   
-  
-    m_rightClimbMotor.set(MathUtil.clamp(m_rightMotorOutput, MAX_DOWN_VOLTAGE,MAX_UP_VOLTAGE )); 
-    m_leftClimbMotor.set(MathUtil.clamp(m_leftMotorOutput, MAX_DOWN_VOLTAGE, MAX_UP_VOLTAGE)); 
+
+    if (m_isRetracting) {
+      m_rightClimbMotor.set(MathUtil.clamp(m_rightMotorOutput, MAX_DOWN_VOLTAGE,MAX_UP_VOLTAGE)); 
+      m_leftClimbMotor.set(MathUtil.clamp(m_leftMotorOutput, MAX_DOWN_VOLTAGE, MAX_UP_VOLTAGE)); 
+    } else {
+      m_rightClimbMotor.set(0);
+      m_leftClimbMotor.set(0);
+    }
     
 }
   
   public void extend(){
-    m_rightPIDController.setSetpoint(EXTEND_MOTOR_SETPIONT);
-    m_leftPIDController.setSetpoint(EXTEND_MOTOR_SETPIONT);
+    m_isRetracting = false;
   }
 
   public void retract() {   
-    m_rightPIDController.setSetpoint(RETRACT_MOTOR_SETPOINT);
-    m_leftPIDController.setSetpoint(RETRACT_MOTOR_SETPOINT);
+    m_isRetracting = true;
   }
  
 public Command getExtendCommand(){
