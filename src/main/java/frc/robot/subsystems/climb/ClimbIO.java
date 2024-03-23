e5293e23f2174c945a54e60f1a8202a2085bb274
LucasF6 <fowler.33148@sbhsd.k12.ca.us>
2024-03-22 21:21:01 -0700

    make climb subsystem use the IO layer

--- src/main/java/frc/robot/subsystems/climb/ClimbIO.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIO.java
@@ -5,24 +5,23 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClimbIO {
     @AutoLog
     public static class ClimbIOInputs {
-        public double leftposition;
-        public double rightposition; 
-        public double leftvoltage = 0.0;
-        public double rightvoltage = 0.0;
-        public double m_leftCurrent = 0.0; 
-        public double m_rightCurrent = 0.0;
-        
+        public double leftPosition = 0.0;
+        public double rightPosition = 0.0; 
+        public double leftCurrent = 0.0; 
+        public double rightCurrent = 0.0;
+        public double leftTemperature = 0.0;
+        public double rightTemperature = 0.0;
     }
     public default void updateInputs(ClimbIOInputs Inputs){}
     
-    public default void setleftAppliedVoltage(double volts) {}
-    public default void setrightAppliedVoltage(double volts){} 
-    
-    public default void stop() {}
-    
-    public default void climb(){
-    
-    }
+    public default void setLeftDutyCycle(double volts) {}
 
+    public default void setRightDutyCycle(double volts) {} 
+
+    public default void setLeftPosition(double position) {}
+
+    public default void setRightPosition(double position) {}
+
+    public default void setBrakeMode(boolean brake) {}
 
 }
--- src/main/java/frc/robot/subsystems/climb/ClimbIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSparkMax.java
@@ -3,34 +3,53 @@ package frc.robot.subsystems.climb;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import static frc.robot.Constants.Climb.L_CLIMB_MOTOR_ID;
 import static frc.robot.Constants.Climb.R_CLIMB_MOTOR_ID;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-public class ClimbIOSparkMax implements ClimbIO {
-    
-    private CANSparkMax m_rightClimbMotor = new CANSparkMax(R_CLIMB_MOTOR_ID,MotorType.kBrushless);
-    private CANSparkMax m_leftClimbMotor = new CANSparkMax(L_CLIMB_MOTOR_ID,MotorType.kBrushless);
-    private RelativeEncoder m_rightClimbEncoder = m_rightClimbMotor.getEncoder();
-    private RelativeEncoder m_leftClimbEncoder = m_leftClimbMotor.getEncoder();
+public class ClimbIOSparkMax implements ClimbIO { 
+  private CANSparkMax m_rightClimbMotor = new CANSparkMax(R_CLIMB_MOTOR_ID,MotorType.kBrushless);
+  private CANSparkMax m_leftClimbMotor = new CANSparkMax(L_CLIMB_MOTOR_ID,MotorType.kBrushless);
+  private RelativeEncoder m_rightClimbEncoder = m_rightClimbMotor.getEncoder();
+  private RelativeEncoder m_leftClimbEncoder = m_leftClimbMotor.getEncoder();
 
-    @Override
-    public void  setleftAppliedVoltage(double volts ){
-        m_leftClimbMotor.set(volts);
-    }
+  @Override
+  public void updateInputs(ClimbIOInputs inputs){
+    inputs.leftPosition = m_leftClimbEncoder.getPosition();
+    inputs.rightPosition = m_rightClimbEncoder.getPosition(); 
+    inputs.leftCurrent = m_leftClimbMotor.getOutputCurrent(); 
+    inputs.rightCurrent = m_rightClimbMotor.getOutputCurrent(); 
+    inputs.leftTemperature = m_leftClimbMotor.getMotorTemperature();
+    inputs.rightTemperature = m_rightClimbMotor.getMotorTemperature();
+  }
 
-    public void  setrightAppliedVoltage(double volts){
-        m_rightClimbMotor.set(volts);
-    }
+  @Override
+  public void setLeftDutyCycle(double percent){
+    m_leftClimbMotor.set(percent);
+  }
 
-    public void updateInputs(ClimbIOInputs Inputs){
-        Inputs.leftposition = m_leftClimbEncoder.getPosition();
-        Inputs.rightposition = m_rightClimbEncoder.getPosition(); 
-        Inputs.leftvoltage = m_leftClimbMotor.getAppliedOutput() * m_leftClimbMotor.getBusVoltage(); 
-        Inputs.rightvoltage = m_rightClimbMotor.getAppliedOutput() * m_rightClimbMotor.getBusVoltage(); 
-        Inputs.m_leftCurrent = m_leftClimbMotor.getOutputCurrent(); 
-        Inputs.m_rightCurrent = m_rightClimbMotor.getOutputCurrent(); 
-    }
-    }
+  @Override
+  public void setRightDutyCycle(double percent){
+    m_rightClimbMotor.set(percent);
+  }
+
+  @Override
+  public void setLeftPosition(double position) {
+    m_leftClimbEncoder.setPosition(position);
+  }
+
+  @Override
+  public void setRightPosition(double position) {
+    m_rightClimbEncoder.setPosition(position);
+  }
+
+  @Override
+  public void setBrakeMode(boolean brake) {
+    m_leftClimbMotor.setIdleMode(brake ? IdleMode.kBrake : IdleMode.kCoast);
+    m_rightClimbMotor.setIdleMode(brake ? IdleMode.kBrake : IdleMode.kCoast);
+  }
+
+}
