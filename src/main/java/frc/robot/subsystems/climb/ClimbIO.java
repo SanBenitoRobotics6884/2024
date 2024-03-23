c93c68e8a202da2ca397f20a4105a41e8724eebd
Voyager6884 <dsr6884voyager@gmail.com>
2024-03-22 19:41:50 -0700

    need more codeing not done

--- src/main/java/frc/robot/subsystems/climb/ClimbIO.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIO.java
@@ -5,21 +5,24 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClimbIO {
     @AutoLog
     public static class ClimbIOInputs {
-        public double leftposition = 0.0;
-        public double rigthpostiton = 0.0; 
+        public double leftposition;
+        public double rightposition; 
         public double leftvoltage = 0.0;
         public double rightvoltage = 0.0;
+        public double m_leftCurrent = 0.0; 
+        public double m_rightCurrent = 0.0;
+        
     }
-
-public default void updateInputs(ClimbIOInputs Inputs) {}
-
-public default void setAppliedVoltage(double volts) {}
-
-public default void stop() {}
-
-
-public default void climb(){}
-
+    public default void updateInputs(ClimbIOInputs Inputs){}
+    
+    public default void setleftAppliedVoltage(double volts) {}
+    public default void setrightAppliedVoltage(double volts){} 
+    
+    public default void stop() {}
+    
+    public default void climb(){
+    
+    }
 
 
 }
--- src/main/java/frc/robot/subsystems/climb/ClimbIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSparkMax.java
@@ -1,13 +1,36 @@
 package frc.robot.subsystems.climb;
 
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+
+import static frc.robot.Constants.Climb.L_CLIMB_MOTOR_ID;
+import static frc.robot.Constants.Climb.R_CLIMB_MOTOR_ID;
+
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ClimbIOSparkMax implements ClimbIO {
-    private final CANSparkMax leader = new CANSparkMax(0, MotorType.kBrushless);
-    private final RelativeEncoder encoder = leader.getEncoder();
     
+    private CANSparkMax m_rightClimbMotor = new CANSparkMax(R_CLIMB_MOTOR_ID,MotorType.kBrushless);
+    private CANSparkMax m_leftClimbMotor = new CANSparkMax(L_CLIMB_MOTOR_ID,MotorType.kBrushless);
+    private RelativeEncoder m_rightClimbEncoder = m_rightClimbMotor.getEncoder();
+    private RelativeEncoder m_leftClimbEncoder = m_leftClimbMotor.getEncoder();
 
-    
-}
+    @Override
+    public void  setleftAppliedVoltage(double volts ){
+        m_leftClimbMotor.set(volts);
+    }
+
+    public void  setrightAppliedVoltage(double volts){
+        m_rightClimbMotor.set(volts);
+    }
+
+    public void updateInputs(ClimbIOInputs Inputs){
+        Inputs.leftposition = m_leftClimbEncoder.getPosition();
+        Inputs.rightposition = m_rightClimbEncoder.getPosition(); 
+        Inputs.leftvoltage = m_leftClimbMotor.getAppliedOutput() * m_leftClimbMotor.getBusVoltage(); 
+        Inputs.rightvoltage = m_rightClimbMotor.getAppliedOutput() * m_rightClimbMotor.getBusVoltage(); 
+        Inputs.m_leftCurrent = m_leftClimbMotor.getOutputCurrent(); 
+        Inputs.m_rightCurrent = m_rightClimbMotor.getOutputCurrent(); 
+    }
+    }
